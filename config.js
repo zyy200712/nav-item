7c3c65c390b7cb46e5c22ea7031887fe21413b19
zkf6 <zkkev53@gmail.com>
2025-10-30 23:41:08 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,11 +2,12 @@ require('dotenv').config();
 
 module.exports = {
   admin: {
-    username: process.env.ADMIN_USERNAME || 'admin',
-    password: process.env.ADMIN_PASSWORD || '123456'
+    username: process.env.ADMIN_USERNAME || 'zcr',
+    password: process.env.ADMIN_PASSWORD || 'zbb'
   },
   server: {
     port: process.env.PORT || 3000,
     jwtSecret: process.env.JWT_SECRET || 'nav-item-jwt-secret-2024-secure-key'
   }
+
 }; 
